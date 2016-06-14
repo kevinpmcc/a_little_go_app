bd5c20281369246528ec20f8d9da4845584023f9
kevinpmcc <kevinpatrickmccarthy@gmail.com>
2016-06-14 21:03:30 +0100

    did section0 and section1

--- Section0-Setup/maths.go
+++ Section0-Setup/maths.go
@@ -1,2 +1,13 @@
 //Write a program that adds two numbers together!
 //Remember package, import, and the main function
+
+package main
+import "fmt"
+
+func add(a int, b int) int {
+  return a + b
+}
+
+func main() {
+  fmt.Println(add(1, 1))
+}
--- Section1-HttpGet/http-get.go
+++ Section1-HttpGet/http-get.go
@@ -2,6 +2,16 @@ package main
 
 // step 1 - use http.Get to send a request and
 // print a response, handling the error
-func main() {
+import (
+        "fmt"
+        "log"
+        "net/http"
+)
 
+func main() {
+  res, err := http.Get("https://golang.org")
+  if err != nil {
+        log.Fatal(err)
+      }
+      fmt.Println(res.Status) 
 }
